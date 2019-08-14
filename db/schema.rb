9734f430ee78272025eef1d05d73de19df6cba1a
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-08-14 16:41:23 -0500

    basic app complete

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_08_13_221231) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
